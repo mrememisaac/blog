04cc79b8e7552b6c8b58be07c50ca370dd7b9ef5
Emem Isaac <me@ememisaac.com>
2021-10-28 23:48:49 +0100

    Implements the delete category feature at the application level

---
+++ blog/EmemIsaac.Blog.Application/Features/Categories/Commands/DeleteCategory/DeleteCategoryCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using EmemIsaac.Blog.Application.Contracts.Persistence;
+using EmemIsaac.Blog.Application.Exceptions;
+using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EmemIsaac.Blog.Application.Features.Categories.Commands.DeleteCategory
+{
+    public class DeleteCategoryCommandHandler : IRequestHandler<DeleteCategoryCommand>
+    {
+        private readonly ICategoryRepository categoryRepository;
+        private readonly IMapper mapper;
+
+        public DeleteCategoryCommandHandler(ICategoryRepository categoryRepository, IMapper mapper)
+        {
+            this.categoryRepository = categoryRepository ?? throw new ArgumentNullException(nameof(categoryRepository));
+            this.mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+
+        public async Task<Unit> Handle(DeleteCategoryCommand request, CancellationToken cancellationToken)
+        {
+            var category = await categoryRepository.GetById(request.CategoryId);
+            if (category == null) throw new NotFoundException("Category", request.CategoryId);
+            await categoryRepository.Delete(category);
+            return Unit.Value;
+        }
+    }
+}
