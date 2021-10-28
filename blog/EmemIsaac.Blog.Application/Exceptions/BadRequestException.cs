4a4cfd03aef6231d47ce6a7f37efb28d55e43752
Emem Isaac <me@ememisaac.com>
2021-10-28 23:47:42 +0100

    Adds custom exceptions

---
+++ blog/EmemIsaac.Blog.Application/Exceptions/BadRequestException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace EmemIsaac.Blog.Application.Exceptions
+{
+    public class BadRequestException : ApplicationException
+    {
+        public BadRequestException(string message) : base(message)
+        { }
+    }
+}
---
+++ blog/EmemIsaac.Blog.Application/Exceptions/ValidationException.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EmemIsaac.Blog.Application.Exceptions
+{
+    public class ValidationException : ApplicationException
+    {
+        public List<string> ValidationErrors { get; private set; } = new List<string>();
+
+        public ValidationException(ValidationResult validationResult)
+        {
+            ValidationErrors = validationResult.Errors.Select(x => x.ErrorMessage).ToList();
+        }
+    }
+}
