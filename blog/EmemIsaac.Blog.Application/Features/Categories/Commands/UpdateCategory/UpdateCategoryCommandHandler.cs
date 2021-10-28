899e48c66331774ddd46adbffa80baa606d295b4
Emem Isaac <me@ememisaac.com>
2021-10-28 23:49:21 +0100

    Implements the update category feature

---
+++ blog/EmemIsaac.Blog.Application/Features/Categories/Commands/UpdateCategory/UpdateCategoryCommandHandler.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using EmemIsaac.Blog.Application.Contracts.Persistence;
+using EmemIsaac.Blog.Application.Exceptions;
+using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EmemIsaac.Blog.Application.Features.Categories.Commands.UpdateCategory
+{
+    public class UpdateCategoryCommandHandler : IRequestHandler<UpdateCategoryCommand>
+    {
+        private readonly ICategoryRepository categoryRepository;
+        private readonly IMapper mapper;
+        private readonly UpdateCategoryCommandValidator validator;
+
+        public UpdateCategoryCommandHandler(ICategoryRepository categoryRepository, IMapper mapper, UpdateCategoryCommandValidator validator)
+        {
+            this.categoryRepository = categoryRepository ?? throw new ArgumentNullException(nameof(categoryRepository));
+            this.mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            this.validator = validator ?? throw new ArgumentNullException(nameof(validator));
+        }
+
+        public async Task<Unit> Handle(UpdateCategoryCommand request, CancellationToken cancellationToken)
+        {
+            var category = await categoryRepository.GetById(request.Id);
+            if (category == null) throw new NotFoundException(nameof(UpdateCategoryModel), request.Id);
+
+            //validate
+            var validationResult = await validator.ValidateAsync(request);
+            if (!validationResult.IsValid) throw new Exceptions.ValidationException(validationResult);
+
+            //update
+            mapper.Map(request, category, typeof(UpdateCategoryCommand), typeof(Domain.Entities.Category));
+            await categoryRepository.Update(category);
+
+            return Unit.Value;
+        }
+    }
+}
---
+++ blog/EmemIsaac.Blog.Application/Features/Categories/Commands/UpdateCategory/UpdateCategoryCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace EmemIsaac.Blog.Application.Features.Categories.Commands.UpdateCategory
+{
+    public class UpdateCategoryCommandValidator : AbstractValidator<UpdateCategoryCommand>
+    {
+
+        private int NameMaximumLength = 20;
+
+        public UpdateCategoryCommandValidator()
+        {
+            RuleFor(p => p.Name)
+                .NotEmpty().WithMessage("{PropertyName} is required")
+                .NotNull()
+                .MaximumLength(NameMaximumLength).WithMessage("{PropertyName} cannot exceed {NameMaximumLength} characters");
+        }
+    }
+}
