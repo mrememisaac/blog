f68a2583658fa01b869a97852882bc99f41da4d5
Emem Isaac <me@ememisaac.com>
2021-10-29 21:49:03 +0100

    Implements the base repository

---
+++ blog/EmemIsaac.Blog.Persistence/Repositories/Repository.cs
@@ -0,0 +1,48 @@
+﻿using EmemIsaac.Blog.Application.Contracts.Persistence;
+using EmemIsaac.Blog.Domain.Common;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace EmemIsaac.Blog.Persistence.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : Entity
+    {
+        protected readonly BlogDbContext context;
+
+        public Repository(BlogDbContext context)
+        {
+            this.context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public async Task<T> Add(T entity)
+        {
+            await context.Set<T>().AddAsync(entity);
+            await context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task Delete(T entity)
+        {
+            context.Set<T>().Remove(entity);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task<T> GetById(Guid id)
+        {
+            return await context.Set<T>().FindAsync(id);
+        }
+
+        public async Task<IReadOnlyList<T>> ListAll()
+        {
+            return await context.Set<T>().ToListAsync();
+        }
+
+        public async Task Update(T entity)
+        {
+            context.Entry(entity).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            await context.SaveChangesAsync();
+        }
+    }
+}
