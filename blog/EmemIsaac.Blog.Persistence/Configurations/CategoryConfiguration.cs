6310339dc3a5a625e49044c6ee74aa6bdffa841b
Emem Isaac <me@ememisaac.com>
2021-10-29 21:48:42 +0100

    Configures the category entity

---
+++ blog/EmemIsaac.Blog.Persistence/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using EmemIsaac.Blog.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EmemIsaac.Blog.Persistence.Configurations
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.Property(p => p.Name)
+                .IsRequired()
+                .HasMaxLength(50);
+            builder.Property(p => p.CreateDate)
+                .IsRequired();
+            builder.Property(p => p.CreatorId)
+                .IsRequired();
+            builder.Property(p => p.Url)
+                .IsRequired()
+                .HasMaxLength(50);    
+        }
+    }
+}
