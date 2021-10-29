9d64ae272fa8146d44323ce602b6e3eb323fce75
Emem Isaac <me@ememisaac.com>
2021-10-29 21:52:17 +0100

    Registers Persistence services

---
+++ blog/EmemIsaac.Blog.Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,21 @@
+﻿using EmemIsaac.Blog.Application.Contracts.Persistence;
+using EmemIsaac.Blog.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EmemIsaac.Blog.Persistence
+{
+    public static class PersistenceServiceRegistration
+    {
+        public static IServiceCollection AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<BlogDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+           
+            services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            
+            return services;
+        }
+    }
+}
