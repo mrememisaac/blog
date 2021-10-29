25378c173d0d67ecdff15dfe06c781062a7be35e
Emem Isaac <me@ememisaac.com>
2021-10-29 21:51:13 +0100

    Implements the Categories DbSet, applies the configuration and setups timestamping

---
+++ blog/EmemIsaac.Blog.Persistence/BlogDbContext.cs
@@ -0,0 +1,43 @@
+﻿using EmemIsaac.Blog.Domain.Common;
+using EmemIsaac.Blog.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EmemIsaac.Blog.Persistence
+{
+    public class BlogDbContext : DbContext
+    {
+        public BlogDbContext(DbContextOptions<BlogDbContext> dbContextOptions) : base(dbContextOptions)
+        { }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(BlogDbContext).Assembly);
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            foreach (var entry in ChangeTracker.Entries<Entity>())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Modified:
+                        entry.Entity.ChangeDate = DateTimeOffset.Now; 
+                        break;
+                    case EntityState.Added:
+                        entry.Entity.CreateDate = DateTimeOffset.Now;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
