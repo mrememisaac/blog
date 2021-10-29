3daa3b3c08b6bd22432f40de0f57d92c293585d0
Emem Isaac <me@ememisaac.com>
2021-10-29 15:08:37 +0100

    Relocates the automapper profiles

---
+++ blog/EmemIsaac.Blog.Application/Profiles/MappingProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using EmemIsaac.Blog.Domain.Entities;
+
+namespace EmemIsaac.Blog.Application.Profiles
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            //Category
+            CreateMap<Features.Categories.Queries.GetCategory.Article, Article>().ReverseMap();
+            CreateMap<Features.Categories.Queries.GetCategory.Category, Category>().ReverseMap();
+            CreateMap<Features.Categories.Queries.GetCategories.GetCategoryModel, Category>().ReverseMap();
+            CreateMap<Features.Categories.Queries.GetCategoriesWithArticles.Category, Category>().ReverseMap();
+            CreateMap<Features.Categories.Commands.CreateCategory.CreateCategoryModel, Category>().ReverseMap();
+            CreateMap<Features.Categories.Commands.CreateCategory.CreateCategoryCommand, Category>().ReverseMap();
+
+            //Comment
+            CreateMap<Features.Comments.Commands.CreateComment.CreateCommentCommand, Category>().ReverseMap();
+            CreateMap<Features.Comments.Commands.CreateComment.CreateCommentModel, Category>().ReverseMap();
+        }
+    }
+}
