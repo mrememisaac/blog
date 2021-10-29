11d74e8cd10c0ef3ca950abf7bd24b0d55d778fb
Emem Isaac <me@ememisaac.com>
2021-10-29 15:10:07 +0100

    Provides a way to check for/prevent comment duplication

--- blog/EmemIsaac.Blog.Application/Contracts/Persistence/Comment/ICommentRepository.cs
+++ blog/EmemIsaac.Blog.Application/Contracts/Persistence/Comment/ICommentRepository.cs
@@ -1,9 +1,11 @@
 ﻿using EmemIsaac.Blog.Domain.Entities;
+using System;
+using System.Threading.Tasks;
 
 namespace EmemIsaac.Blog.Application.Contracts.Persistence
 {
     public interface ICommentRepository : IRepository<Comment>
     {
-
+        Task<Comment> GetByContentAuthorAndArticle(string content, string authorId, Guid articleId);
     }
 }
