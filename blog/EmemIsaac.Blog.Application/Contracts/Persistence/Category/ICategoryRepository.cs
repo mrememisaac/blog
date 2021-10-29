7d99af7b4c56b654158adec1b08eda789881fdf5
Emem Isaac <me@ememisaac.com>
2021-10-29 15:06:56 +0100

    Returns tasks instead

--- blog/EmemIsaac.Blog.Application/Contracts/Persistence/Category/ICategoryRepository.cs
+++ blog/EmemIsaac.Blog.Application/Contracts/Persistence/Category/ICategoryRepository.cs
@@ -1,9 +1,12 @@
 ﻿using EmemIsaac.Blog.Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace EmemIsaac.Blog.Application.Contracts.Persistence
 {
     public interface ICategoryRepository : IRepository<Category>
     {
-        Category GetByName(string name);
+        Task<Category> GetByName(string name);
+        Task<IReadOnlyCollection<Category>> GetCategoriesWithArticles();
     }
 }
