7cfe206beddf06e473224fe41132f5641d9d79c3
Emem Isaac <me@ememisaac.com>
2023-07-03 12:54:34 +0100

    Implemented essential methods

--- blog/EmemIsaac.Blog.Application/Contracts/Persistence/Article/IArticleRepository.cs
+++ blog/EmemIsaac.Blog.Application/Contracts/Persistence/Article/IArticleRepository.cs
@@ -1,9 +1,14 @@
 ﻿using EmemIsaac.Blog.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace EmemIsaac.Blog.Application.Contracts.Persistence
 {
     public interface IArticleRepository : IRepository<Article>
     {
+        Task<Article> GetArticleByUrl(string url);
 
+        Task<IReadOnlyList<Article>> GetArticles(int page = 1, int numberOfRecords = 50);
     }
 }
