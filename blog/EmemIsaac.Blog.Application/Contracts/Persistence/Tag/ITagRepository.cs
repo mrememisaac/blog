a15fd245ce3048c502d0da79944187b6394a325c
Emem Isaac <me@ememisaac.com>
2023-07-03 12:55:17 +0100

    For direct tag data management

---
+++ blog/EmemIsaac.Blog.Application/Contracts/Persistence/Tag/ITagRepository.cs
@@ -0,0 +1,9 @@
+﻿using EmemIsaac.Blog.Domain.Entities;
+
+namespace EmemIsaac.Blog.Application.Contracts.Persistence.Tag
+{
+    public interface ITagRepository : IRepository<Section>
+    {
+
+    }
+}
