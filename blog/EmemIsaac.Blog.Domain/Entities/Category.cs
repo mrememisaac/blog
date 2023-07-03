19a19792950d805f689e0493c823786c66c0f852
Emem Isaac <me@ememisaac.com>
2023-07-03 13:10:43 +0100

    Modified to ensure valid construction

--- blog/EmemIsaac.Blog.Domain/Entities/Category.cs
+++ blog/EmemIsaac.Blog.Domain/Entities/Category.cs
@@ -11,13 +11,15 @@ namespace EmemIsaac.Blog.Domain.Entities
 
         public ICollection<Article> Articles { get; set; }
 
-        private Category()
-        {
+        //private Category() { }
 
-        }
 
         public Category(string name)
         {
+            if (string.IsNullOrWhiteSpace(name) || string.IsNullOrEmpty(name))
+            {
+                throw new System.ArgumentException($"'{nameof(name)}' cannot be null, empty or whitespace.", nameof(name));
+            }
             Name = name;
         }
     }
