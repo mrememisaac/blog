bf227e6015f7934622e1b0272c9f8ab396a10964
Emem Isaac <me@ememisaac.com>
2023-07-03 13:11:38 +0100

    Support for article tags

---
+++ blog/EmemIsaac.Blog.Domain/Entities/Tag.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EmemIsaac.Blog.Domain.Entities
+{
+    public class Tag
+    {
+        public Guid Id { get; set; }
+        
+        public Guid ArticleId { get; set; }
+
+        public string Name { get; set; }
+    }
+}
