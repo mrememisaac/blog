8b5faa8ad04b650d76ee9050a74308dd81f83567
Emem Isaac <me@ememisaac.com>
2023-07-03 13:18:04 +0100

    Adds tests for verifying mapping profiles

---
+++ EmemIsaac.Blog.Domain.Tests/Articles/MappingProfileTests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace EmemIsaac.Blog.Application.UnitTests.Articles
+{
+    public class MappingProfileTests : ArticleTestsBase
+    {
+        [Fact]
+        public void ValidateMappingConfigurationTest()
+        {
+            mapper.ConfigurationProvider.AssertConfigurationIsValid();
+        }
+    }
+}
