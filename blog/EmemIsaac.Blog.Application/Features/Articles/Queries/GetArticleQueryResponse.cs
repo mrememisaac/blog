5af3fd7f6760255cfb6229f4e9becfa2b45cf3bd
Emem Isaac <me@ememisaac.com>
2023-07-03 13:01:10 +0100

    Adds the shared response for Get Article queries

---
+++ blog/EmemIsaac.Blog.Application/Features/Articles/Queries/GetArticleQueryResponse.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EmemIsaac.Blog.Application.Features.Articles.Queries
+{
+    public class GetArticleQueryResponse
+    {
+        public Guid Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public string Url { get; set; }
+
+        public string Description { get; set; }
+
+        public string Category { get; set; }
+
+        public DateTimeOffset CreateDate { get; set; }
+
+        public string AuthorName { get; set; }
+
+        public string Stage { get; set; }
+
+        public ICollection<ArticleSection> Sections { get; set; }
+
+        public ICollection<ArticleComment> Comments { get; set; }
+
+        public ICollection<ArticleTag> Tags { get; set; }
+    }
+}
