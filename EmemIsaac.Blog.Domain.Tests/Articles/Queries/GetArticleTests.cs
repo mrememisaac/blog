c7780a583b949943914f1cd13420df0e143feed9
Emem Isaac <me@ememisaac.com>
2023-07-03 13:17:28 +0100

    Adds tests for getting and listing articles

---
+++ EmemIsaac.Blog.Domain.Tests/Articles/Queries/GetArticleTests.cs
@@ -0,0 +1,40 @@
+﻿using EmemIsaac.Blog.Application.Features.Articles.Queries.GetArticleById;
+using EmemIsaac.Blog.Application.Features.Articles.Queries.GetArticleByUrl;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Shouldly;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace EmemIsaac.Blog.Application.UnitTests.Articles.Queries
+{
+    public class GetArticleTests : ArticleTestsBase
+    {
+        [Fact]
+        public async Task Can_Retrieve_Article_By_Id()
+        {
+            var query = new GetArticleByIdQuery { Id = Guid.Parse("{B0788D2F-8003-43C1-92A4-EDC76A7C5DDE}") };
+            var logger = new Mock<ILogger<GetArticleByIdQueryHandler>>().Object;
+            var handler = new GetArticleByIdQueryHandler(mapper, articleRepository, logger);
+
+            var article = await handler.Handle(query, cancellationToken);
+
+            article.ShouldNotBeNull();
+            article.Id.ShouldBeEquivalentTo(query.Id);
+        }
+
+        [Fact]
+        public async Task Can_Retrieve_Article_By_Url()
+        {
+            var query = new GetArticleByUrlQuery { Url = "sample-article" };
+            var logger = new Mock<ILogger<GetArticleByUrlQueryHandler>>().Object;
+            var handler = new GetArticleByUrlQueryHandler(mapper, articleRepository, logger);
+
+            var article = await handler.Handle(query, cancellationToken);
+
+            article.ShouldNotBeNull();
+            article.Url.ShouldBeEquivalentTo(query.Url);
+        }
+    }
+}
---
+++ EmemIsaac.Blog.Domain.Tests/Articles/Queries/ListArticleTests.cs
@@ -0,0 +1,25 @@
+﻿using EmemIsaac.Blog.Application.Features.Articles.Queries.ListArticles;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Shouldly;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace EmemIsaac.Blog.Application.UnitTests.Articles.Queries
+{
+    public class ListArticleTests : ArticleTestsBase
+    {
+        [Fact]
+        public async Task Can_List_Articles()
+        {
+            var request = new ListArticlesQuery { Count = 1};
+            var logger = new Mock<ILogger<ListArticlesQueryHandler>>().Object;
+            var handler = new ListArticlesQueryHandler(mapper, logger, articleRepository);
+
+            var articles = await handler.Handle(request, cancellationToken);
+
+            articles.Count().ShouldBe(1);
+        }
+    }
+}
