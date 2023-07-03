58d1bedfc3269d09acb629468be438cf990ebed4
Emem Isaac <me@ememisaac.com>
2023-07-03 12:59:52 +0100

    Implements the Get Article by Url feature

---
+++ blog/EmemIsaac.Blog.Application/Features/Articles/Queries/GetArticleByUrl/GetArticleByUrlQuery.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using System;
+
+namespace EmemIsaac.Blog.Application.Features.Articles.Queries.GetArticleByUrl
+{
+    public class GetArticleByUrlQuery : IRequest<GetArticleQueryResponse>
+    {
+        public string Url { get; set; }
+
+        public bool IncludeSections { get; set; } = true;
+
+        public bool IncludeComments { get; set; } = true;
+
+        public bool IncludeTags { get; set; } = true;
+    }
+}
---
+++ blog/EmemIsaac.Blog.Application/Features/Articles/Queries/GetArticleByUrl/GetArticleByUrlQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using EmemIsaac.Blog.Application.Contracts.Persistence;
+using EmemIsaac.Blog.Application.Features.Articles.Queries.GetArticleById;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EmemIsaac.Blog.Application.Features.Articles.Queries.GetArticleByUrl
+{
+    public class GetArticleByUrlQueryHandler : IRequestHandler<GetArticleByUrlQuery, GetArticleQueryResponse>
+    {
+        private readonly IMapper mapper;
+        private readonly IArticleRepository articleRepository;
+        private readonly ILogger<GetArticleByUrlQueryHandler> logger;
+
+        public GetArticleByUrlQueryHandler(IMapper mapper, IArticleRepository articleRepository, ILogger<GetArticleByUrlQueryHandler> logger)
+        {
+            this.mapper = mapper;
+            this.articleRepository = articleRepository;
+            this.logger = logger;
+        }
+
+        public async Task<GetArticleQueryResponse> Handle(GetArticleByUrlQuery request, CancellationToken cancellationToken)
+        {
+            var article = await articleRepository.GetArticleByUrl(request.Url);
+            return mapper.Map<GetArticleQueryResponse>(article);
+        }
+    }
+}
