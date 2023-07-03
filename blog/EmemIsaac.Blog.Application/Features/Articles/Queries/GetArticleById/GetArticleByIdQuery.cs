3d37c04fc5883cebe4ef5cd6c21e16a22c9e6a5b
Emem Isaac <me@ememisaac.com>
2023-07-03 12:59:40 +0100

    Implements the Get Article By Id feature

---
+++ blog/EmemIsaac.Blog.Application/Features/Articles/Queries/GetArticleById/GetArticleByIdQuery.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using System;
+
+namespace EmemIsaac.Blog.Application.Features.Articles.Queries.GetArticleById
+{
+    public class GetArticleByIdQuery : IRequest<GetArticleQueryResponse>
+    {
+        public Guid Id { get; set; }
+
+        public bool IncludeSections { get; set; } = true;
+
+        public bool IncludeComments { get; set; } = true;
+
+        public bool IncludeTags { get; set; } = true;
+    }
+}
---
+++ blog/EmemIsaac.Blog.Application/Features/Articles/Queries/GetArticleById/GetArticleByIdQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using EmemIsaac.Blog.Application.Contracts.Persistence;
+using EmemIsaac.Blog.Domain.Entities;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using System.Collections;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EmemIsaac.Blog.Application.Features.Articles.Queries.GetArticleById
+{
+    public class GetArticleByIdQueryHandler : IRequestHandler<GetArticleByIdQuery, GetArticleQueryResponse>
+    {
+        private readonly IMapper mapper;
+        private readonly IArticleRepository articleRepository;
+        private readonly ILogger<GetArticleByIdQueryHandler> logger;
+
+        public GetArticleByIdQueryHandler(IMapper mapper, IArticleRepository articleRepository, ILogger<GetArticleByIdQueryHandler> logger)
+        {
+            this.mapper = mapper;
+            this.articleRepository = articleRepository;
+            this.logger = logger;
+        }
+
+        public async Task<GetArticleQueryResponse> Handle(GetArticleByIdQuery request, CancellationToken cancellationToken)
+        {
+            var article = await articleRepository.GetById(request.Id);
+            return mapper.Map<GetArticleQueryResponse>(article);
+        }
+    }
+}
