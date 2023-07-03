6b392b170a63878d78845c6e94c9afc0d0df2fee
Emem Isaac <me@ememisaac.com>
2023-07-03 12:58:49 +0100

    Implements the delete article feature

---
+++ blog/EmemIsaac.Blog.Application/Features/Articles/Commands/DeleteArticle/DeleteArticleCommand.cs
@@ -0,0 +1,14 @@
+﻿using EmemIsaac.Blog.Application.Responses;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EmemIsaac.Blog.Application.Features.Articles.Commands.DeleteArticle
+{
+    public class DeleteArticleCommand : IRequest<BaseResponse>
+    {
+        public Guid ArticleId { get; set; }
+    }
+}
---
+++ blog/EmemIsaac.Blog.Application/Features/Articles/Commands/DeleteArticle/DeleteArticleCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using EmemIsaac.Blog.Application.Contracts.Persistence;
+using EmemIsaac.Blog.Application.Responses;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EmemIsaac.Blog.Application.Features.Articles.Commands.DeleteArticle
+{
+    public class DeleteArticleCommandHandler : IRequestHandler<DeleteArticleCommand, BaseResponse>
+    {
+        private readonly ILogger<DeleteArticleCommandHandler> logger;
+        private readonly IArticleRepository articleRepository;
+
+        public DeleteArticleCommandHandler(ILogger<DeleteArticleCommandHandler> logger, IArticleRepository articleRepository)
+        {
+            this.logger = logger;
+            this.articleRepository = articleRepository;
+        }
+
+        public async Task<BaseResponse> Handle(DeleteArticleCommand request, CancellationToken cancellationToken)
+        {
+            var article = await articleRepository.GetById(request.ArticleId);
+            if (article == null)
+            {
+                var msg = $"Article with Id: {request.ArticleId} does not exist";
+                logger.LogError(msg);
+                throw new Exceptions.BadRequestException(msg);
+            }
+            await articleRepository.Delete(article);
+            return new BaseResponse($"Article with Id: {request.ArticleId} deleted");
+        }
+    }
+}
