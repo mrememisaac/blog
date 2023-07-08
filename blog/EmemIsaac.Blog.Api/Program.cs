028b53374f824ab055a8e753b108255afe3decc9
Emem Isaac <me@ememisaac.com>
2023-07-08 09:12:47 +0100

    Merges Startup into Program

--- blog/EmemIsaac.Blog.Api/Program.cs
+++ blog/EmemIsaac.Blog.Api/Program.cs
@@ -1,26 +1,58 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using EmemIsaac.Blog.Api.Services;
+using EmemIsaac.Blog.Application;
+using EmemIsaac.Blog.Persistence;
+using EmemIsaac.Blog.Application.Contracts;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
+using System;
+using EmemIsaac.Blog.Api;
 
-namespace EmemIsaac.Blog.Api
+var builder = WebApplication.CreateBuilder(args);
+
+var services = builder.Services;
+services.AddApplicationServices();
+services.AddPersistenceServices(builder.Configuration);
+services.AddHttpContextAccessor();
+services.AddScoped<ILoggedInUserService, LoggedInUserService>();
+services.AddControllers();
+services.AddEndpointsApiExplorer();
+services.AddSwaggerGen(c =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "EmemIsaac.Blog.Api", Version = "v1" });
+});
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "EmemIsaac.Blog.Api v1"));
 }
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler();
+}
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+if (app.Environment.IsDevelopment())
+{
+    await app.ResetDatabaseAsync();
+    await app.SeedDatabase();
+}
+
+app.Run();
+
+public partial class Program { }
+
