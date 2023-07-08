b10b80d6a2d47ee9364b82a208d972e4c3d424e5
Emem Isaac <me@ememisaac.com>
2023-07-08 09:10:31 +0100

    Implements repository

---
+++ blog/EmemIsaac.Blog.Persistence/Repositories/CommentRepository.cs
@@ -0,0 +1,21 @@
+﻿using EmemIsaac.Blog.Application.Contracts.Persistence;
+using EmemIsaac.Blog.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmemIsaac.Blog.Persistence.Repositories
+{
+    public class CommentRepository : Repository<Comment>, ICommentRepository
+    {
+        public CommentRepository(BlogDbContext context) : base(context)
+        {
+        }
+
+        public async Task<Comment> GetByContentAuthorAndArticle(string content, string authorId, Guid articleId)
+        {
+            return await context.Comments.FirstOrDefaultAsync(c => c.Content.Equals(content, StringComparison.InvariantCultureIgnoreCase) && c.CreatorId == authorId && c.ArticleId == articleId);
+        }
+    }
+}
