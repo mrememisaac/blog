b3cd11a3bcd6f26206e6d56813c6f20700924acc
Emem Isaac <me@ememisaac.com>
2023-07-08 09:05:04 +0100

    Adds validation

--- blog/EmemIsaac.Blog.Domain/Entities/Tag.cs
+++ blog/EmemIsaac.Blog.Domain/Entities/Tag.cs
@@ -4,6 +4,8 @@ namespace EmemIsaac.Blog.Domain.Entities
 {
     public class Tag
     {
+        public static int NameMaximumLength = 50;
+
         public Guid Id { get; set; }
         
         public Guid ArticleId { get; set; }
