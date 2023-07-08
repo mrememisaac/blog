a9ddf27e0e27c497d816c9b47f86e84f3faf9c3d
Emem Isaac <me@ememisaac.com>
2023-07-08 08:57:14 +0100

    Adds profile mapping updates

--- blog/EmemIsaac.Blog.Application/Profiles/CommentProfiles.cs
+++ blog/EmemIsaac.Blog.Application/Profiles/CommentProfiles.cs
@@ -15,7 +15,7 @@ namespace EmemIsaac.Blog.Application.Profiles
                 .ForMember(d => d.ModifierId, opt => opt.Ignore())
                 .ForMember(d => d.Article, opt => opt.Ignore())
                 .ReverseMap();
-            CreateMap<Features.Comments.Commands.CreateComment.CreateCommentModel, Comment>()
+            CreateMap<Features.Comments.Commands.CreateComment.CreateCommentCommandResponse, Comment>()
                 .ForMember(d => d.Id, opt => opt.Ignore())
                 .ForMember(d => d.CreatorId, opt => opt.Ignore())
                 .ForMember(d => d.CreateDate, opt => opt.Ignore())
@@ -23,7 +23,8 @@ namespace EmemIsaac.Blog.Application.Profiles
                 .ForMember(d => d.ModifierId, opt => opt.Ignore())
                 .ForMember(d => d.Article, opt => opt.Ignore())
                 .ReverseMap();
-            CreateMap<Comment, Features.Articles.ArticleComment>();
+            CreateMap<Comment, Features.Articles.ArticleComment>()
+                .ForMember(d => d.AuthorId, o => o.MapFrom(s => s.CreatorId));
         }
     }
 }
