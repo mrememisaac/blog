804b92bd757f982ae0b74b5ee2fdd95812c28412
Emem Isaac <me@ememisaac.com>
2023-07-08 09:06:25 +0100

    Implements articles repository

---
+++ blog/EmemIsaac.Blog.Persistence/Repositories/ArticleRepository.cs
@@ -0,0 +1,26 @@
+﻿using EmemIsaac.Blog.Application.Contracts.Persistence;
+using EmemIsaac.Blog.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmemIsaac.Blog.Persistence.Repositories
+{
+    public class ArticleRepository : Repository<Article>, IArticleRepository
+    {
+        public ArticleRepository(BlogDbContext context) : base(context)
+        { 
+        }
+
+        public async Task<Article> GetArticleByUrl(string url)
+        {
+            return await context.Articles.FirstOrDefaultAsync(x => x.Url == url);
+        }
+
+        public async Task<IReadOnlyList<Article>> GetArticles(int page = 0, int numberOfRecords = 50)
+        {
+            return await context.Articles.Skip(page*numberOfRecords).Take(numberOfRecords).ToListAsync();
+        }
+    }
+}
