59c9e6b7ece01a9f8ca7c2fdff5a3a3312dbb909
Emem Isaac <me@ememisaac.com>
2023-07-12 14:55:39 +0100

    Adds tag view models

---
+++ blog/EmemIsaac.Blog.BApplication/ViewModels/TagViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace EmemIsaac.Blog.BApplication.ViewModels
+{
+    public class TagViewModel
+    {
+        public Guid Id { get; set; }
+
+        public Guid ArticleId { get; set; }
+
+        public string Name { get; set; }
+    }
+}
