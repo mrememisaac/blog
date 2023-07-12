4fe5e6bcf7e02780caf4bb7226f1c56fdb2f2136
Emem Isaac <me@ememisaac.com>
2023-07-12 14:51:29 +0100

    Adds Web API Client

---
+++ blog/EmemIsaac.Blog.BApplication/Services/Base/Client.cs
@@ -0,0 +1,24 @@
+﻿namespace EmemIsaac.Blog.BApplication.Services.Base
+{
+    public partial class Client : IClient
+    {
+        public HttpClient HttpClient
+        {
+            get
+            {
+                return _httpClient;
+            }
+        }
+    }
+
+    public partial class ServiceClient : IClient
+    {
+        public HttpClient HttpClient
+        {
+            get
+            {
+                return _httpClient;
+            }
+        }
+    }
+}
