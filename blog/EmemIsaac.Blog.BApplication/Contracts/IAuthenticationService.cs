865bc966120409f8ed9596b6e64d82762e636ad5
Emem Isaac <me@ememisaac.com>
2023-07-12 14:37:02 +0100

    Adds Authentication service contracts

---
+++ blog/EmemIsaac.Blog.BApplication/Contracts/IAuthenticationService.cs
@@ -0,0 +1,11 @@
+﻿namespace EmemIsaac.Blog.BApplication.Contracts
+{
+    public interface IAuthenticationService
+    {
+        Task<bool> Authenticate(string email, string password);
+
+        Task<bool> Register(string firstName, string lastName, string userName, string email, string password);
+
+        Task Logout();
+    }
+}
