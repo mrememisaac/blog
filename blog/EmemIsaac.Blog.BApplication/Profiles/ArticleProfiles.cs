f46d83b670086e7d1aa611c48b4cf27b7ef28818
Emem Isaac <me@ememisaac.com>
2023-07-12 14:42:08 +0100

    Adds article mappings

---
+++ blog/EmemIsaac.Blog.BApplication/Profiles/ArticleProfiles.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+
+namespace EmemIsaac.Blog.BApplication.Profiles
+{
+    public class ArticleProfiles : Profile
+    {
+        public ArticleProfiles()
+        {
+            CreateMap<Services.Base.ListArticlesQueryResponse, ViewModels.Articles.ArticlesListItemViewModel>();
+            CreateMap<Services.Base.GetArticleQueryResponse, ViewModels.Articles.ArticleViewModel>();
+            CreateMap<ViewModels.Articles.CreateArticleViewModel, Services.Base.CreateArticleCommand>();
+            CreateMap<ViewModels.Articles.ArticleViewModel, Services.Base.CreateArticleCommandResponse>();
+            CreateMap<ViewModels.Articles.UpdateArticleViewModel, Services.Base.UpdateArticleCommand>();
+        }
+    }
+}
