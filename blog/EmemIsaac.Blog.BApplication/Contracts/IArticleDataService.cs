015a2d9d617a1daaa7e612cafd7a5f4eb3a67f42
Emem Isaac <me@ememisaac.com>
2023-07-12 14:28:25 +0100

    Adds the article data service contract

---
+++ blog/EmemIsaac.Blog.BApplication/Contracts/IArticleDataService.cs
@@ -0,0 +1,21 @@
+﻿using EmemIsaac.Blog.BApplication.Services.Base;
+using EmemIsaac.Blog.BApplication.ViewModels.Articles;
+
+
+namespace EmemIsaac.Blog.BApplication.Contracts
+{
+    public interface IArticleDataService
+    {
+        Task<IEnumerable<ArticlesListItemViewModel>> GetAllArticles();
+        
+        Task<ArticleViewModel> GetArticleByUrl(string url);
+        
+        Task<ArticleViewModel> GetArticleById(Guid id);
+        
+        Task<ApiResponse<CreateArticleCommandResponse>> CreateArticle(CreateArticleViewModel article);
+        
+        Task<ApiResponse<Guid>> UpdateArticle(UpdateArticleViewModel article);
+
+        Task DeleteArticle(Guid id);
+    }
+}
