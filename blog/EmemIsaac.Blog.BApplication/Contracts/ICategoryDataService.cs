ed0cd1d7c8b7661f2d0a3e66cee0df2e79a9ac5c
Emem Isaac <me@ememisaac.com>
2023-07-12 14:37:27 +0100

    Adds category service contract

---
+++ blog/EmemIsaac.Blog.BApplication/Contracts/ICategoryDataService.cs
@@ -0,0 +1,19 @@
+﻿using EmemIsaac.Blog.BApplication.ViewModels.Categories;
+
+namespace EmemIsaac.Blog.BApplication.Contracts
+{
+    public interface ICategoryDataService
+    {
+        Task DeleteCategory(Guid id);
+        
+        Task UpdateCategory(CategoryViewModel category);
+        
+        Task<CategoryViewModel> GetCategory(Guid id);
+        
+        Task<CategoryViewModel> CreateCategory(CategoryViewModel category);
+        
+        Task<IEnumerable<CategoryViewModel>> GetAllCategories();
+        
+        Task<IEnumerable<CategoryWithArticlesViewModel>> GetCategoriesWithArticles();
+    }
+}
