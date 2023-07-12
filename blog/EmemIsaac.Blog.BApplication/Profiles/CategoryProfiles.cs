5be098bc1f452523a4c643cbebc45a3a8c5a60fb
Emem Isaac <me@ememisaac.com>
2023-07-12 14:45:04 +0100

    Adds category mappings

---
+++ blog/EmemIsaac.Blog.BApplication/Profiles/CategoryProfiles.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+
+namespace EmemIsaac.Blog.BApplication.Profiles
+{
+    public class CategoryProfiles : Profile
+    {
+        public CategoryProfiles()
+        {
+            CreateMap<Services.Base.GetCategoriesWithArticlesQueryResponse, ViewModels.Categories.CategoryWithArticlesViewModel>();
+            CreateMap<ViewModels.Categories.CategoryViewModel, Services.Base.CreateCategoryCommand>();
+            CreateMap<ViewModels.Categories.CategoryViewModel, Services.Base.CreateCategoryCommandResponse>().ReverseMap();
+            CreateMap<ViewModels.Categories.CategoryViewModel, Services.Base.UpdateCategoryCommand>();
+            CreateMap<ViewModels.Categories.CategoryViewModel, Services.Base.GetCategoryModel>().ReverseMap();
+        }
+    }
+}
