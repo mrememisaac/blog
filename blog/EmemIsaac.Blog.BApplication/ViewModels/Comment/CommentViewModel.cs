941d8e7ee8ee04dd2e261c357f77a81e3b60e642
Emem Isaac <me@ememisaac.com>
2023-07-12 14:55:12 +0100

    Adds comment view models

---
+++ blog/EmemIsaac.Blog.BApplication/ViewModels/Comment/CommentViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace EmemIsaac.Blog.BApplication.ViewModels.Comment
+{
+    public class CommentViewModel
+    {
+        public Guid ParentId { get; set; }
+
+        public string Content { get; set; }
+
+        public Guid ArticleId { get; set; }
+
+        public string AuthorName { get; set; }
+    }
+}
