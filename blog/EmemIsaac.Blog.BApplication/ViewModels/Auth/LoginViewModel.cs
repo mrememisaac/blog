ba319ecb484f7b23ec090e9f47276ae3eeaac483
Emem Isaac <me@ememisaac.com>
2023-07-12 14:54:28 +0100

    Adds authentication view models

---
+++ blog/EmemIsaac.Blog.BApplication/ViewModels/Auth/LoginViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace EmemIsaac.Blog.BApplication.ViewModels.Auth
+{
+    public class LoginViewModel
+    {
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
