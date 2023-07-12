abf832a383c9a711339741152b6f5e344dfa1552
Emem Isaac <me@ememisaac.com>
2023-07-12 15:57:19 +0100

    Displays articles on home page

---
+++ blog/EmemIsaac.Blog.BApplication/Pages/Index.razor.cs
@@ -0,0 +1,31 @@
+﻿using EmemIsaac.Blog.BApplication.Contracts;
+using EmemIsaac.Blog.BApplication.ViewModels.Articles;
+using EmemIsaac.Blog.BApplication.ViewModels.Categories;
+using Microsoft.AspNetCore.Components;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace EmemIsaac.Blog.BApplication.Pages
+{
+    public partial class Index
+    {
+        [Inject]
+        public IArticleDataService ArticleDataService { get; set; }
+
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        public string Message { get; set; }
+
+        public ObservableCollection<ArticlesListItemViewModel> Articles { get; set; } = new ObservableCollection<ArticlesListItemViewModel>();
+
+        [Required]
+        public Guid SelectedArticleId { get; set; }
+
+        protected async override Task OnInitializedAsync()
+        {
+            var list = await ArticleDataService.GetAllArticles();
+            Articles = new ObservableCollection<ArticlesListItemViewModel>(list);
+        }
+    }
+}
