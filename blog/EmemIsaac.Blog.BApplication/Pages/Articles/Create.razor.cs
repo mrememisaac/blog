dc9c8f10c0f7a76a2b18712d3f89dfd7bbc8a731
Emem Isaac <me@ememisaac.com>
2023-07-12 15:42:56 +0100

    Adds the articles create page

---
+++ blog/EmemIsaac.Blog.BApplication/Pages/Articles/Create.razor.cs
@@ -0,0 +1,68 @@
+﻿using EmemIsaac.Blog.BApplication.Contracts;
+using EmemIsaac.Blog.BApplication.Services.Base;
+using EmemIsaac.Blog.BApplication.ViewModels.Articles;
+using EmemIsaac.Blog.BApplication.ViewModels.Categories;
+using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace EmemIsaac.Blog.BApplication.Pages.Articles
+{
+    public partial class Create
+    {
+        [Inject]
+        public ICategoryDataService CategoryDataService { get; set; }
+
+        [Inject]
+        public IArticleDataService ArticleDataService { get; set; }
+
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        public CreateArticleViewModel CreateArticleViewModel { get; set; }
+
+        public string Message { get; set; }
+
+        public ObservableCollection<CategoryViewModel> Categories { get; set; } = new ObservableCollection<CategoryViewModel>();
+
+        [Required]
+        public Guid SelectedCategoryId { get; set; }
+
+        protected async override Task OnInitializedAsync()
+        {
+            CreateArticleViewModel = new CreateArticleViewModel();
+            var list = await CategoryDataService.GetAllCategories();
+            Categories = new ObservableCollection<CategoryViewModel>(list);
+        }
+
+        protected async Task HandleValidSubmit()
+        {
+            CreateArticleViewModel.CategoryId = SelectedCategoryId;
+            var response = await ArticleDataService.CreateArticle(CreateArticleViewModel);
+            HandleResponse(response);
+        }
+
+        private void HandleResponse(ApiResponse<CreateArticleCommandResponse> response)
+        {
+            if (response.Success)
+            {
+                Message = "Article added";
+            }
+            else
+            {
+                Message = response.Message;
+                if (!string.IsNullOrEmpty(response.ValidationErrors))
+                    Message += response.ValidationErrors;
+            }
+        }
+
+        private Dictionary<string, object> EditorConf = new Dictionary<string, object> {
+            { "menubar", true },
+            { "plugins", "link image code advcode" },
+            { "toolbar", "undo redo | styleselect | forecolor | bold italic | alignleft aligncenter alignright alignjustify | outdent indent | link image | code" }
+        };
+
+        private const string ApiKey = "kdjaflkdjdlkdlkdlkjdlkdsfajlkdsdkjl";
+    }
+}
