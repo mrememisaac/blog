2fbcd45f3c9ff35f7db9f2f03b158131f6b77ccf
Emem Isaac <me@ememisaac.com>
2023-07-12 14:54:54 +0100

    Adds category view models

---
+++ blog/EmemIsaac.Blog.BApplication/ViewModels/Categories/CategoryViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace EmemIsaac.Blog.BApplication.ViewModels.Categories
+{
+    public class CategoryViewModel
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Url { get; private set; }
+    }
+}
---
+++ blog/EmemIsaac.Blog.BApplication/ViewModels/Categories/CategoryWithArticlesViewModel.cs
@@ -0,0 +1,13 @@
+﻿using EmemIsaac.Blog.BApplication.ViewModels.Articles;
+
+namespace EmemIsaac.Blog.BApplication.ViewModels.Categories
+{
+    public class CategoryWithArticlesViewModel
+    {
+        public string Name { get; set; }
+
+        public string Url { get; private set; }
+
+        public ICollection<ArticlesListItemViewModel> Articles { get; set; }
+    }
+}
